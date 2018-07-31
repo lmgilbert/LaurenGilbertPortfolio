baa43c778a1ac50ba4c8edf076d7798f19e8a77b
Lauren <laurengilbert1997@gmail.com>
2018-07-31 12:05:40 +1200

    Spelling mistake

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,7 @@ $('#return-to-top').click(function() {      // When arrow is clicked
         scrollTop : 0                       // Scroll to top of body
     }, 500);
 });
+
+$(function() {
+    $('.jscroll').jscroll();    
+});
